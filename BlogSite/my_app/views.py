9922d45c04a0ed751bbfefa9476202132ecffc3f
knp13 <80236055+knp13@users.noreply.github.com>
2021-03-16 13:24:03 +0530

    Create views.py

---
+++ BlogSite/my_app/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.http import HttpResponse
+from .models import Post, Comment
+from django.contrib.auth.models import User
+from .forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView,DetailView,
+                                CreateView,UpdateView, DeleteView)
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'my_app/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'my_app/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'my_app/about.html', {'title':'About'})
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post-detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'my_app/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post-detail',pk=post_pk)
+
+
+# def home(request):
+#     context={
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'my_app/home.html', context)
